f193368a9cb83a3713b0a5df70e8c3148d58a243
Just_a_Mango <horace.hoff@gmail.com>
2022-03-26 20:46:15 +0100

    Add files via upload

--- pass-gen.cpp
+++ pass-gen.cpp
@@ -1,15 +1,20 @@
+// Imports the required modules
 #include <iostream>
 #include <string>
 int main() {
+  // Prints the title and asks the user for the desired length of their password
   std::cout << "== PASS-GEN ==\n";
   std::cout << "[+] Desired password length: ";
   int password_length;
   std::cin >> password_length;
+  // Defines the variable for the final password and the list which contains all characters allowed in the password
   std::string final_pass;
   char chars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!/*-+.&'(-_)=^$ù*:;,}]@^`|[{#";
+  // Generates a random character and appends it to the final password
   for (int index = 0; index <= password_length; index++) {
     final_pass = final_pass + chars[rand() % 91];
   }
+  // Prints the final password
   std::cout << final_pass;
   return 0;
 }
--- pass-gen.py
+++ pass-gen.py
@@ -1,9 +1,15 @@
+# Imports the string and random modules
 import string, random
+# Prints the title
 print("== PASS-GEN ==")
+# Asks the user for a desired password length and then converting it to an integer
 length = int(input("[+] Desired password length: "))
+# Initial values for the variables
 index = 0
 final_pass = ""
+# This is a loop that will continue until the index is greater than the length
 while index <= length:
     final_pass = final_pass + list(string.printable)[random.randint(0, 99)]
     index += 1
+# Replaces the spaces in the final password with nothing, and then prints the final password
 print("\nPassword:\n"+final_pass.replace(" ",""))
